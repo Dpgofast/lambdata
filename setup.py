71acf508e353f139ece4080df381e62c1b462adf
Dpgofast <43558343+Dpgofast@users.noreply.github.com>
2019-02-12 14:45:06 -0600

    updated version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.MD', 'r') as fh:
 
 setuptools.setup(
     name = 'lambdata_Dpgofast',
-    version = '0.0.2',
+    version = '0.0.3',
     author = 'Dpgofast',
     description = ' A collection of Data Science helper functions',
     long_description = LONG_DESCRIPTION,
