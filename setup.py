778151e6eab5821e0da314802dd6dcce77fb953b
Dpgofast <43558343+Dpgofast@users.noreply.github.com>
2019-02-12 19:44:48 -0600

    no message for you

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ with open('README.MD', 'r') as fh:
     LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
-    name = 'lambdata_Dpgofast',
-    version = '0.0.4',
+    name = 'lambdataDpgofast',
+    version = '0.0.4.1',
     author = 'Dpgofast',
     description = ' A collection of Data Science helper functions',
     long_description = LONG_DESCRIPTION,
