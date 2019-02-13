2d01ffa8257f2969bbae8e6717d38b0c4d4394d6
Dpgofast <43558343+Dpgofast@users.noreply.github.com>
2019-02-13 15:48:42 -0600

    Corrected spacing

--- setup.py
+++ setup.py
@@ -13,17 +13,17 @@ with open('README.MD', 'r') as fh:
     LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
-    name = 'lambdataDpgofast',
-    version = '0.0.4.1',
-    author = 'Dpgofast',
-    description = ' A collection of Data Science helper functions',
-    long_description = LONG_DESCRIPTION,
-    long_descrition_content_type = 'text/markdown',
-    url = 'https://github.com/Dpgofast/lambdata',
-    packages = setuptools.find_packages(),
-    python_requires = '>=3.5',
-    install_requires = REQUIRED,
-    classifiers = [
+    name='lambdataDpgofast',
+    version='0.0.4.1',
+    author='Dpgofast',
+    description=' A collection of Data Science helper functions',
+    long_description=LONG_DESCRIPTION,
+    long_descrition_content_type='text/markdown',
+    url='https://github.com/Dpgofast/lambdata',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.5',
+    install_requires=REQUIRED,
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
